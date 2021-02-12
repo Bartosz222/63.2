d492f2e139a40cbc66042946e778ac520d00cd42
Bartosz222 <62373647+Bartosz222@users.noreply.github.com>
2021-02-12 12:51:18 +0100

    1

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+ifstream wejscie;
+ofstream wyjscie;
+bool wynik=true;
+bool sprawdzPlik() {
+    if (wejscie.good() && wyjscie.good()) return true;
+    else return false;
+}
+bool wyszukaj(string sprawdzana) {
+    char liczba = sprawdzana[0];
+    for (int i = 1; i < sprawdzana.length(); i++) {
+        if (liczba == sprawdzana[i]&&liczba == '1') {
+        	return false;
+        }
+        liczba = sprawdzana[i];
+    }
+    return true;
+}
+int main()
+{
+    wejscie.open("ciagi.txt");
+    wyjscie.open("wynik.txt");
+    string ciag;
+    if (sprawdzPlik() == true) {
+        while (!wejscie.eof()) {
+            wejscie >> ciag;
+            if (wyszukaj(ciag) == true) {
+                cout << ciag << endl;
+                wyjscie << ciag << endl;
+            }
+        }
+        wejscie.close();
+        wyjscie.close();
+    }
+    else exit(0);
+    return 0;
+}
+
